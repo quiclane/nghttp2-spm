caa69fc4f4d255d5c240877aa0eb93d6955c6f11
quiclane <developer@veridanlayerseven.com>
2026-02-07 22:21:53 +0200

    spm: add binary target package

--- Package.swift
+++ Package.swift
@@ -1,2 +1,3 @@
+// swift-tools-version: 5.9
 import PackageDescription
-let package=Package(name:"nghttp2-spm",products:[.library(name:"NGHTTP2",targets:["NGHTTP2"])],targets:[.target(name:"NGHTTP2",path:"Sources/NGHTTP2")])
+let package=Package(name:"nghttp2-spm",platforms:[.iOS(.v13)],products:[.library(name:"nghttp2",targets:["nghttp2"])],targets:[.binaryTarget(name:"nghttp2",url:"https://github.com/quiclane/nghttp2-spm/releases/download/ios-20260208-dcb6842fb673/NGHTTP2.xcframework.zip",checksum:"774c7667b74583ff4eb30acbaa1228bcc4edb6f1cca1010d5a1384c42a8521e4")])
